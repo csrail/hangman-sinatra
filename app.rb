8d47b78061d6c6a5d1c2a783b2e0fbb18880394b
Cornelius Soon <cornelius.soon@gmail.com>
2016-09-17 19:56:03 +1200

    Setup methods talking

--- app.rb
+++ app.rb
@@ -1,9 +1,58 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+enable :sessions
+
+configure do
+  generate_secret_word
+end
+
 get '/' do
   
-  erb :app
+  erb :app, :locals => { :testing => "",
+                         :secret_word => secret_word,
+                         :feedback1 => @feedback1,
+                         :feedback2 => @feedback2
+                       }
+end
+
+post '/' do
+  session['guess'] = params['guess']
+  validate_input
+  
+  erb :app, :locals => { :testing => session['guess'],
+                         :feedback1 => @feedback1,
+                         :feedback2 => @feedback2
+                        }
+end
+
+
+
+def validate_input
+  letters_only
+  one_character_only
+end
+
+def letters_only
+  if !("a".."z").include? session['guess']
+    @feedback1 = "That's not a letter."
+    @feedback2 = "Only searching for letters:"
+  end
+end
+
+def one_character_only
+  if session['guess'].length != 1
+    @feedback1 = "That's not quite one character."
+    @feedback2 = "Only searching for one character:"
+  end
+end
+
+def secret_word
+  @@secret_word
+end
+
+def generate_secret_word
+  @@secret_word = word_bank.strip.split(//)
 end
 
 def word_bank
